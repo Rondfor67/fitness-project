247ddf47849fa4f1f93c4fc35b94bf2ecb99eb6e
Rondfor67 <marinakushakova@icloud.com>
2026-04-03 02:54:54 +0500

    fix port

--- server/server.js
+++ server/server.js
@@ -379,6 +379,8 @@ app.post("/api/login", async (req, res) => {
   });
 });
 
-app.listen(5000, () => {
-  console.log("Server started on 5000");
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log("Server started on " + PORT);
 });
